3e8701bf152d06b830b28253df8dc589aa4700cf
wuyuming <1048121795@qq.com>
2024-07-12 09:11:05 +0800

    完成重置密码功能

--- my-project-backend/src/main/java/com/example/controller/AccountController.java
+++ my-project-backend/src/main/java/com/example/controller/AccountController.java
@@ -6,9 +6,12 @@ import com.example.entity.dto.AccountDetails;
 import com.example.entity.vo.request.ChangePasswordVO;
 import com.example.entity.vo.request.DetailsSaveVO;
 import com.example.entity.vo.request.ModifyEmailVO;
+import com.example.entity.vo.request.PrivacySaveVO;
 import com.example.entity.vo.response.AccountDetailsVO;
+import com.example.entity.vo.response.AccountPrivacyVO;
 import com.example.entity.vo.response.AccountVO;
 import com.example.service.AccountDetailsService;
+import com.example.service.AccountPrivacyService;
 import com.example.service.AccountService;
 import com.example.utils.Const;
 import jakarta.annotation.Resource;
@@ -28,6 +31,8 @@ public class AccountController {
 
     @Resource
     AccountDetailsService detailsService;
+    @Resource
+    AccountPrivacyService privacyService;
 
     @GetMapping("/info")
     public RestBean<AccountVO> info(@RequestAttribute(Const.ATTR_USER_ID) int id){
@@ -60,6 +65,18 @@ public class AccountController {
     public RestBean<Void> changePassword(@RequestAttribute(Const.ATTR_USER_ID) int id,
                                          @RequestBody @Valid ChangePasswordVO vo){
         return this.messageHandle(() -> service.changePassword(id, vo));
+    }
+
+    @PostMapping("/save-privacy")
+    public RestBean<Void> savePrivacy(@RequestAttribute(Const.ATTR_USER_ID) int id,
+                                      @RequestBody @Valid PrivacySaveVO vo){
+        privacyService.savePrivacy(id, vo);
+        return RestBean.success();
+    }
+
+    @GetMapping("/privacy")
+    public RestBean<AccountPrivacyVO> privacy(@RequestAttribute(Const.ATTR_USER_ID) int id){
+        return RestBean.success(privacyService.accountPrivacy(id).asViewObject(AccountPrivacyVO.class));
     }
 
     /**
--- my-project-frontend/src/views/settings/PrivacySetting.vue
+++ my-project-frontend/src/views/settings/PrivacySetting.vue
@@ -3,7 +3,7 @@
 import Card from "@/components/Card.vue";
 import {Setting, Switch} from "@element-plus/icons-vue";
 import {reactive, ref} from "vue";
-import {post} from "@/net";
+import {get, post} from "@/net";
 import {ElMessage} from "element-plus";
 
 const form = reactive({
@@ -50,18 +50,47 @@ function resetPassword(){
   //   }
   // })
 }
+
+const saving = ref(true)
+const privacy = reactive({
+  phone: false,
+  wx: false,
+  qq: false,
+  email: false,
+  gender: false
+})
+get('/api/user/privacy', data => {
+  privacy.phone = data.phone
+  privacy.wx = data.wx
+  privacy.qq = data.qq
+  privacy.email = data.email
+  privacy.gender = data.gender
+  saving.value = false;
+})
+function savePrivacy(type, status){
+  saving.value = true
+  post('/api/user/save-privacy', {type, status}, () => {
+    ElMessage.success('隐私设置司改成功!')
+    saving.value = false
+  })
+}
 </script>
 
 <template>
   <div style="max-width: 600px;margin: auto;">
     <div style="margin-top: 20px">
-      <card :icon="Setting" title="隐私设置" desc="在这里设置哪些内容可以被其他人看到，请各位小伙伴注重自己的隐私">
+      <card :icon="Setting" title="隐私设置" desc="在这里设置哪些内容可以被其他人看到，请各位小伙伴注重自己的隐私" v-loading="saving">
         <div class="checkbox-list">
-          <el-checkbox>公开展示我的手机号</el-checkbox>
-          <el-checkbox>公开展示我的电子邮件地址</el-checkbox>
-          <el-checkbox>公开展示我的微信号</el-checkbox>
-          <el-checkbox>公开展示我的QQ号</el-checkbox>
-          <el-checkbox>公开展示我的性别</el-checkbox>
+          <el-checkbox @change="savePrivacy('phone', privacy.phone)"
+                       v-model="privacy.phone">公开展示我的手机号</el-checkbox>
+          <el-checkbox @change="savePrivacy('email', privacy.email)"
+                       v-model="privacy.email">公开展示我的电子邮件地址</el-checkbox>
+          <el-checkbox @change="savePrivacy('wx', privacy.wx)"
+                       v-model="privacy.wx">公开展示我的微信号</el-checkbox>
+          <el-checkbox @change="savePrivacy('qq', privacy.qq)"
+                       v-model="privacy.qq">公开展示我的QQ号</el-checkbox>
+          <el-checkbox @change="savePrivacy('gender', privacy.gender)"
+                       v-model="privacy.gender">公开展示我的性别</el-checkbox>
         </div>
       </card>
       <card style="margin: 20px 0" :icon="Setting"
